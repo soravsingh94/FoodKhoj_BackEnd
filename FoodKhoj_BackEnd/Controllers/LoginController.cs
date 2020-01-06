2137abb76d74681aa1e84ed357abfd9c637f99c3
Sorav Singh <sorav.singh@nagarro.com>
2020-01-06 12:48:27 +0530

    Add project files.

---
+++ FoodKhoj_BackEnd/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodKhoj_BackEnd.Controllers
+{
+    [ApiController]
+    [Route("api/login")]
+    public class LoginController : ControllerBase
+    {
+        //private IFoodKhojContext _dbContext;
+
+        //public LoginController(IFoodKhojContext dbContext)
+        //{
+        //    _dbContext = dbContext;
+        //}
+
+        [Route("loginuser")]
+        [HttpGet]
+        public IActionResult LoginUser()
+        {
+            return Ok();
+        }
+
+        //[Route("registeruser")]
+        //[HttpPost]
+        //public IActionResult RegisterUser(User model)
+        //{
+        //    IActionResult response = Unauthorized();
+        //    var data = _dbContext.Insert_User(model);
+        //    response = Ok(new { data });
+
+        //    return response;
+        //}
+    }
+}
---
+++ FoodKhoj_BackEnd/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FoodKhoj_BackEnd
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+           // services.AddDbContext<FoodKhojContext>(options => options.UseSqlServer(DesEncrypter.Decrypt(Configuration["ConnectionString:Key"])));
+            services.AddControllers();
+            //services.AddScoped<IFoodKhojContext, FoodKhojContext>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
